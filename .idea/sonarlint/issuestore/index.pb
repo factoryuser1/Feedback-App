0a48a809dde0a40f610e79c2a8a6f88188146b71
factoryuser1 <factoryuser1@yahoo.com>
2022-02-20 13:25:32 -0600

    Created initial components, props and state

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-<
-package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
 =
 src/index.css,c\5\c5d38a62d1faae90a2acaa34b79a7ef365c917e4
 A
--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import React from 'react';
+import Header from "./components/Header";
 
 function App(props) {
 
-    const title = 'Blog Post';
-    const body = 'This is my blog post';
-    const comments =[
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'}
-    ]
-    const loading = false;
-    if(loading) return <h1>Loading...</h1>
-
-    const showComments = true;
-
-    const commentBlock = (
-        <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-            {
-                comments.map((comment, index) =>(
-                            <li key={index}>
-                                Index is: {index}, Commend id is: {comment.id} and Comment is: {comment.text}
-                            </li>
-                        )
-                    )
-                }
-            </ul>
-        </div>
-    )
-
     return (
-        <div className='container'>
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-            {showComments && commentBlock}
-        </div>
+        <>
+            <Header text={true}/>
+            <div className='container'>
+                <h1>My App</h1>
+            </div>
+        </>
+
     );
 }
 
