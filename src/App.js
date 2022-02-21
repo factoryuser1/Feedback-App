6685f34433dea840c6a92cfda31f5c6bb0c2d2b4
factoryuser1 <factoryuser1@yahoo.com>
2022-02-21 13:55:37 -0600

    Added PropTypes and Formatted Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { useState} from "react";
+import {useState} from "react";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
-
-
+import Card from "./components/shared/Card";
 
 function App() {
-
     const [feedback, setFeedback] = useState(FeedbackData);
-
     return (
         <>
-            <Header />
+            <Header/>
             <div className='container'>
                 <FeedbackList feedback={feedback}/>
+                {/*<Card>This is my Card Styled Component Example</Card>*/}
             </div>
         </>
-
     );
 }
 
