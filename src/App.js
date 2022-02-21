50cbdaed9a85a16848ccaaefe9caa0f6d7e4d5d7
factoryuser1 <factoryuser1@yahoo.com>
2022-02-21 11:17:36 -0600

    Created FeedbackList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useState} from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
 
 
-function App(props) {
+
+function App() {
+
+    const [feedback, setFeedback] = useState(FeedbackData);
 
     return (
         <>
             <Header />
             <div className='container'>
-                <FeedbackItem />
+                <FeedbackList feedback={feedback}/>
             </div>
         </>
 
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import {useState} from "react";
 
-function FeedbackItem(props) {
-    const [rating, setRating] = useState(7);
-    const [text, setText] = useState('This is an example of feedback item.')
-
-    const handleClick = () => {
-        setRating((prev) => {
-            console.log(prev);
-            return prev +1;
-        });
-
-    };
-
+function FeedbackItem({item}) {
     return (
         <div className='card'>
-            <div className="num-display">{rating}</div>
-                <div className="text-display">
-                    {text}
-                </div>
-            <button onClick={handleClick}>Click</button>
+            <div className="num-display">{item.rating}</div>
+                <div className="text-display">{item.text}</div>
         </div>
     );
 }
